42711da2151c5102d335034c6bf76d9ffb87543a
prashantgmicro <pgole31@gmail.com>
2020-11-13 14:31:55 +0530

    home page

---
+++ index.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Knee pain clinic</title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  	<link rel="stylesheet" href="css/style.css">
+  	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">  	
+</head>
+<body>
+	<!----------------Header Start------------------->
+	<section class="navbr">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 text-left pad_0">
+					<div><img src="img/Knee-Pain-Clinic-Web.png" alt="Logo"></div>
+				</div>
+				<div class="col-sm-6 text-right pad_0">
+					<div>
+						<a href="tel:(123)-456-789" class="Btn_S">(123)-456-789</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="navbr">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 text-left pad_0">
+					<ul>
+						<li class="active"><a href="">Home</a></li>
+						<li><a href="">About</a></li>
+						<li><a href="">Testimonials</a></li>
+						<li><a href="">News</a></li>
+						<li><a href="">Contact</a></li>
+					</ul>
+				</div>
+				<div class="col-sm-6 text-right pad_0">
+					<div>
+						<a href="tel:123-456-789" class="Btn_S1">set up a free info-visit</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!----------------Header End------------------->
+	<!----------------banner Start------------------->
+	<section class="banner">
+		<div class="inner_banner">
+			<img src="img/severe-knee-pain.jpg" alt="banner">
+			<div class="PosCon">
+				<h1>Is Knee Pain Holding You Back?</h1>
+				<iframe width="100%" height="315" src="https://www.youtube.com/embed/20tcGNTRcjU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				<h3>A Free Pre-Consultation is waiting for you!</h3>
+				<a href="#" class="Btn_S2">Click to Call Now!</a>
+			</div>
+		</div>
+	</section>
+	<!----------------banner End------------------->
+	<!----------------small banner Start------------------->
+	<section class="small_banner clrr">
+		<div class="row">
+			<div class="col-sm-9"><h3>Learn more about our Knee Pain Relief Program and schedule your FREE Info-Visit Today!</h3></div>
+			<div class="col-sm-3"><a href="tel:(123) 456-7890" class="Btn_S3">Call us Now! (123) 456-7890</a></div>
+		</div>
+	</section>
+	<!----------------small banner End------------------->
+	<!----------------left right section Start------------------->
+	<section class="LftRight">
+		<div class="container">
+			<div class="row">
+			<div class="col-sm-6 borrd">
+				<img src="img/avoid-knee-surgery-in-mcallen-tx-750x750.jpg" alt="banner">
+			</div>
+			<div class="col-sm-6">
+				<h2>The Knee Pain Clinic</h2>
+				<p><b>Knee Pain Clinic</b> offers a pain relief treatment to those patients that suffer from degenerative arthritis of the knee. This condition is also known as knee osteoarthritis. Certainly, the majority of our patients that have suffered from this condition have tried several anti-inflammatory and painkillers. However, these patients continue to experience knee pain, leading other doctors to suggest knee surgery as your only option.</p>
+				<p>Our team physicians and medical assistants will assist you through your knee pain relief journey. Dr. Manuel Sanchez has a natural FDA Approved Solution that will help relieve the pain and avoid surgery in just 5 sessions. With the help of state-of-the-art technology, our health care providers inject a natural visco-supplementation formula called Hyaluronic Acid. This formula will give the natural lubrication and nourish to your cartilage. Therefore, allowing us to relieve you from the pain your knee is causing and decrease inflammation. Best of all, this treatment is covered by the majority of insurance plans, including Medicare.</p>
+			</div>
+		</div>
+		</div>		
+	</section>
+	<!----------------left right section End------------------->
+	<!----------------small banner Start------------------->
+	<section class="small_banner clrb">
+		<div class="row">
+			<div class="col-sm-12 text-center"><h3>Schedule your FREE Pre-Consultation Today and Receive a FREE Report on How to Find Relief Today</h3></div>
+		</div>
+	</section>
+	<!----------------small banner End------------------->
+	<!----------------left right section Start------------------->
+	<section class="LftRight2">
+		<div class="row">			
+			<div class="col-sm-6">
+				<div class="PAdLbx">
+					<h2 class="text-center">How Knee Pain Clinic Works</h2>
+					<p class="text-center">The great outcome of our treatment has to do with our method</p>
+					<div class="row Mart50">
+						<div class="col-sm-6">
+							<div class="row">
+								<div class="col-sm-2 pad_0">
+									<div class="IcoLR"></div>
+								</div>
+								<div class="col-sm-10">
+									<div class="COnLR">
+										<h5>Info-Visit</h5>
+										<p>A member of our staff will run a quick survey to identify if this treatment is best for you. Followed by explaining the procedure in detail, run your insurance or explain the costs of our treatment.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="row">
+								<div class="col-sm-2 pad_0">
+									<div class="IcoLR"></div>
+								</div>
+								<div class="col-sm-10">
+									<div class="COnLR">
+										<h5>Treatment</h5>
+										<p>After confirming Knee Osteoarthritis, our patients start the treatment. During this process, you will be numbed in order to apply our injection, with the help of fluoroscopy.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row Mart50">
+						<div class="col-sm-6">
+							<div class="row">
+								<div class="col-sm-2 pad_0">
+									<div class="IcoLR"></div>
+								</div>
+								<div class="col-sm-10">
+									<div class="COnLR">
+										<h5>Evaluation</h5>
+										<p>Our patients are evaluated through a series of on-site studies to determine the current condition of the knee. Through this, our physician will determine the number of treatments our patient requires.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="row">
+								<div class="col-sm-2 pad_0">
+									<div class="IcoLR"></div>
+								</div>
+								<div class="col-sm-10">
+									<div class="COnLR">
+										<h5>Physical Therapy</h5>
+										<p>The patient is able to recover motion on their knees by performing a series of exercises and electrotherapy with the help of our licensed physical therapist’s team.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>				
+			</div>
+			<div class="col-sm-6 pad_0">
+				<img src="img/mcallen_knee_pain_clinic.jpg" alt="banner">
+			</div>
+		</div>	
+	</section>
+	<!----------------left right section End------------------->
+	<!----------------left right section Start------------------->
+	<section class="LftRight">
+		<div class="container">
+			<div class="row">
+			<div class="col-sm-6 wfu text-center">
+				<img src="img/Cowboys-Cheerleader-Carrie-OBrien-Sibley-555x740.jpg" alt="banner">
+			</div>
+			<div class="col-sm-6 wfu">
+				<h3>There's a Relief for Everyone!</h3>
+				<p>Former cheerleader Carrie O’Brien-Sibley became our patient at the Knee Pain Clinic after several years serving the Cowboys team. Despite exercising is recommended to reduce the risk of knee osteoarthritis (OA). However, the several routines of dancing, jumping and cheering for the team, lead Carrie to suffer from OA due to a sports injury. It’s not easy for an active person to lose the motion on their legs due to severe pain.  For this reason, Carrie came to Dr. Manuel Sanchez and the Knee Pain Clinic to stop the pain and continue with her busy schedule without pain!</p>
+			</div>
+		</div>
+		</div>		
+	</section>
+	<!----------------left right section End------------------->
+	<!----------------left right section Start------------------->
+	<section class="LftRight2 bacimg">
+		<div class="row">			
+			<div class="col-sm-6 text-center">
+				<div class="PAdLbx">
+					<h3>REAL PEOPLE, REAL RESULTS</h3>
+					<div class="testmonialSlider">
+						<div>
+							<div class="testBox">
+								<img src="img/knee-pain-clinic-testimonials-2-150x150.jpg" alt="">
+								<h5>I feel really great!</h5>
+								<p>I came to Dr. Sanchez’ Knee Pain Clinic because I was in unbearable pain, I could not do much, I couldn’t walk a lot. Now that I have my two knees treated, I can walk again, I can wear high heels, I can dance, I feel really great!</p>
+								<span>- Sophia Hogg</span>
+							</div>
+						</div>
+						<div></div>
+						<div></div>
+					</div>
+				</div>				
+			</div>
+			<div class="col-sm-6 pad_0">
+				<img src="img/knee-pain-clinic-results.jpg" alt="banner">
+			</div>
+		</div>	
+	</section>
+	<!----------------left right section End------------------->
+	<!----------------small banner Start------------------->
+	<section class="small_banner clry">
+		<div class="row">
+			<div class="col-sm-9"><h3>Schedule your FREE Pre-Consultation Today and Receive a FREE Report on How to Find Relief Today</h3></div>
+			<div class="col-sm-3"><a href="tel:(123) 456-7890" class="Btn_S5">Make An Appointment</a></div>
+		</div>
+	</section>
+	<!----------------small banner End------------------->
+	<!----------------contact form Start------------------->
+	<section class="contactfrm">
+		<img src="img/find-knee-pain-relief-mcallen-tx.jpg" alt="banner">
+		<div class="ContactBox">
+			<form action="#">
+				<div class="row">
+					<div class="col-sm-6">
+						<label for="">First Name</label>
+						<input type="text" class="form-control">
+					</div>
+					<div class="col-sm-6">
+						<label for="">Last Name</label>
+						<input type="text" class="form-control">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-6">
+						<label for="">Phone Number</label>
+						<input type="text" class="form-control">
+					</div>
+					<div class="col-sm-6">
+						<label for="">Email</label>
+						<input type="text" class="form-control">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-6">
+						<label for="">Preffer Date</label>
+						<input type="text" class="form-control">
+					</div>
+					<div class="col-sm-6">
+						<label for="">Available Time</label>
+						<select name="" id="" class="form-control">
+							<option value="">Please Select Time</option>
+							<option value="Morning">Morning</option>
+							<option value="Afternoon">Afternoon</option>
+						</select>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-6">
+						<button type="submit" class="Btn_S1 Mart25">Request a free info visit</button>
+					</div>
+					<div class="col-sm-6"></div>
+				</div>
+			</form>
+		</div>
+	</section>
+	<!----------------contact form End------------------->
+	<!----------------footer Start------------------->
+	<section class="footer_sec">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+					<div>
+						<img src="img/Knee-Pain-Clinic-Web.png" alt="Logo">
+						<h6>13988 Diplomat Drive, <br>Suite 100 Farmers Branch, <br>TX 75234</h6>
+						<p>Phone : <a href="tel:(469)-805-4561">(469)-805-4561</a> </p>
+						<p>Fax : (469) 709-1948</p>
+						<a href="#" class="Btn_S3 Mart25">Make An Appointment</a>
+					</div>
+				</div>
+				<div class="col-sm-3">
+					<ul>
+						<li><h4>About</h4></li>
+						<li><a href="">Home</a></li>
+						<li><a href="">About</a></li>
+						<li><a href="">Testimonials</a></li>
+						<li><a href="">News</a></li>
+						<li><a href="">Contact</a></li>
+					</ul>
+				</div>
+				<div class="col-sm-3">
+					<ul>
+						<li><h4>Other</h4></li>
+						<li><a href="#">Privacy Policy</a></li>
+						<li><a href="#">Terms & Conditions</a></li>
+						<li><a href="#">Clinic</a></li>
+						<li><a href="#">Blog</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="copyrightMrk">
+		<div class="container">
+			<p>Copyright © Knee Pain Clinic & Manuel Sanchez, MD 2020. All Rights Reserved | Designed By <a href="https://ibridgedigital.com/">Ibridge Digital</a></p>
+		</div>		
+	</section>
+	<!----------------footer End------------------->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
